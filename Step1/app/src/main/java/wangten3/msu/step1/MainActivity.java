6c781f41215121025299a83dbce6b559421613d1
wangtengjiao1997 <48685426+wangtengjiao1997@users.noreply.github.com>
2019-12-27 14:14:22 -0500

    Add files via upload

---
+++ Step1/app/src/main/java/wangten3/msu/step1/MainActivity.java
@@ -0,0 +1,14 @@
+package wangten3.msu.step1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
